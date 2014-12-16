7c2d1d40633cbaaf570bdc5105c0d07f9feb7902
Shawna Spoor <shawna@spoor.org>
2014-12-16 18:55:50 -0500

    fixed a formatting issue with the jplayer audio

--- js/project3.js
+++ js/project3.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
       $("#jquery_jplayer_1").jPlayer({
         ready: function () {
           $(this).jPlayer("setMedia", {
-            title: "Big Buck Bunny Trailer",
-            m4v: "http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v",
-            ogv: "http://www.jplayer.org/video/ogv/Big_Buck_Bunny_Trailer.ogv",
-            poster: "http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
+            title: "Space is awesome",
+            m4v: "video.mp4",
+            ogv: "video.ogv",
+            poster: "images/moonRise.jpg"
           });
         },
         cssSelectorAncestor: "#jp_container_1",
@@ -23,9 +23,9 @@ $(document).ready(function(){
       $("#jquery_jplayer_2").jPlayer({
         ready: function () {
           $(this).jPlayer("setMedia", {
-            title: "Bubble",
-            m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+            title: "Jen saying Hi",
+            m4a: "audio.mp3",
+            oga: "audio.ogv"
           });
         },
         cssSelectorAncestor: "#jp_container_2",
@@ -188,62 +188,5 @@ $(document).ready(function(){
 		$('#canvas').clearCanvas();
 	});
 	//end clear canvas
-/*
-		//make the smile larger on mouse roll over
-		
-		/*$('#canvas').drawArc({
-			  layer: true,
-			  name: 'smile',
-			  fillStyle: '#000',
-			  strokeStyle: '#000',
-			  strokeWidth: 5,
-			  x: 150, y: 140,
-			  radius: 50,
-			  start: 90, end: 270
-			});
-		*/
-		/*
-	$('#canvas').drawText({
-		  layer:true,
-		  name:'text',
-		  fillStyle: '#000',
-		  strokeStyle: '#000',
-		  strokeWidth: 1,
-		  x: 300, y: 20,
-		  fontSize: 30,
-		  fontFamily: 'Georgia, serif',
-		  text: 'Paint me like one of your french girls'
-		});
-		$('#canvas').animateLayer(0, {
-				x:300, 
-				y:350
-				}, 2000);
-		
-	$('#smiley-button').click( function (){
-	// Draw a smiley face, also chained methofds
-	$('#canvas').drawPolygon({
-	  strokeStyle: '#000',
-	  strokeWidth: 6,
-	  x: 100, y: 100,
-	  radius: 25,
-	  sides: 3
-	}).drawPolygon({
-		  strokeStyle: '#000',
-		  strokeWidth: 6,
-		  x: 200, y: 100,
-		  radius: 25,
-		  sides: 3
-		}).drawArc({
-			  strokeStyle: '#000',
-			  strokeWidth: 5,
-			  x: 150, y: 140,
-			  radius: 50,
-			  start: 90, end: 230
-			});
-		//end draw smiley face
-	});
-	//end click smiley function
 
-		
-*/
 });
